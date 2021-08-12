942c3734c42e6693647887e8509b99f4ed92b01d
Igor Harchenko <1305hariok@ukr.net>
2021-08-12 12:04:25 +0300

    Выполнено дз №12, добавлена незначительная стилизация

---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export const getRefs = () => {
+    return {
+        countryInfo: document.querySelector('.country-info'),
+        countryList: document.querySelector('.country-list'),
+        searchCountry: document.querySelector('#search-box'),
+    };
+};
